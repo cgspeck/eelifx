43b7d6d7c46c60938d9c7cb56228d523e76a0f63
Chris Speck <cgspeck@gmail.com>
2017-03-28 19:06:42 +1100

    A loop which displays light status every couple seconds

--- eelifx/main.py
+++ eelifx/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import asyncio as aio
 import aiolifx as alix
@@ -6,14 +7,15 @@ from time import sleep
 from bulbs import Bulbs
 
 UDP_BROADCAST_PORT = 56700
-MyBulbs = Bulbs()
+
 
 
 def print_status(loop, bulbs):
     print(MyBulbs.bulbs)
-    loop.call_later(5, print_status, loop)
+    loop.call_later(5, print_status, loop, bulbs)
 
 
+MyBulbs = Bulbs()
 loop = aio.get_event_loop()
 coro = loop.create_datagram_endpoint(
             partial(alix.LifxDiscovery, loop, MyBulbs),
@@ -23,10 +25,10 @@ coro = loop.create_datagram_endpoint(
 try:
     # loop.add_reader(sys.stdin, readin)
     server = loop.create_task(coro)
-    # loop.call_soon(print_status, loop, MyBulbs)
+    loop.call_soon(print_status, loop, MyBulbs)
     print("Hit \"Enter\" to start")
     print("Use Ctrl-C to quit")
-    loop.run_forever(print_status)
+    loop.run_forever()
 except:
     pass
 finally:
