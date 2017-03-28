4bfc03a4e2fcfeb38f1a208d7b9c41cbd2455cae
Chris Speck <cgspeck@gmail.com>
2017-03-28 22:20:20 +1100

    wip: start function to apply blub state

--- eelifx/lifx_commander.py
+++ eelifx/lifx_commander.py
@@ -1,10 +1,11 @@
+import re
 from functools import wraps
 
 
 def run_once(f):
     @wraps(f)
     def wrapper(*args, **kwds):
-        if f.__name__ in args[0]._command_stack.keys():
+        if f.__name__ in args[0]._command_stack:
             return None
 
         return f(*args, **kwds)
@@ -12,13 +13,14 @@ def run_once(f):
 
 
 class LifxCommander():
-    def __init__(self, ee_poll_interval, max_luminance):
+    def __init__(self, ee_poll_interval, max_luminance, target_group='.*'):
         self._ee_poll_interval = ee_poll_interval
         self._max_luminance = max_luminance
         self._command_stack = {}
         self._supported_effects = ['none', 'breathe', 'flicker']
+        self._target_group = re.compile(target_group, flags=re.I)
 
-    def reset_commands(self):
+    def reset(self):
         self._command_stack = {}
 
     @run_once
@@ -36,3 +38,9 @@ class LifxCommander():
         assert val in self._supported_effects
         print('setting effect')
         self._command_stack['set_effect'] = val
+
+    def apply(self, blubs):
+        for bulb in bulbs:
+            if re.match(bulb.group):
+                if 'set_hue' in self._command_stack:
+                    pass
