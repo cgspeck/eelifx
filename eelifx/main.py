963b090300644e1b03ccf273de150cd75b1b1a0f
Chris Speck <cgspeck@gmail.com>
2017-03-28 18:47:45 +1100

    WIP figuring out asyncio

---
+++ eelifx/main.py
@@ -0,0 +1,35 @@
+import sys
+import asyncio as aio
+import aiolifx as alix
+from functools import partial
+from time import sleep
+from bulbs import Bulbs
+
+UDP_BROADCAST_PORT = 56700
+MyBulbs = Bulbs()
+
+
+def print_status(loop, bulbs):
+    print(MyBulbs.bulbs)
+    loop.call_later(5, print_status, loop)
+
+
+loop = aio.get_event_loop()
+coro = loop.create_datagram_endpoint(
+            partial(alix.LifxDiscovery, loop, MyBulbs),
+            local_addr=('0.0.0.0', UDP_BROADCAST_PORT)
+        )
+
+try:
+    # loop.add_reader(sys.stdin, readin)
+    server = loop.create_task(coro)
+    # loop.call_soon(print_status, loop, MyBulbs)
+    print("Hit \"Enter\" to start")
+    print("Use Ctrl-C to quit")
+    loop.run_forever(print_status)
+except:
+    pass
+finally:
+    server.cancel()
+    loop.remove_reader(sys.stdin)
+    loop.close()
